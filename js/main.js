c91b59d469acd17956c8335e9a62b9a4404b3f16
Admin <admin@Admins-MacBook-Air.local>
2019-04-13 21:10:09 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -2,20 +2,28 @@ console.log("Up and running!");
  
  var cards =["queen","queen","king","king"];
  var cardsInPlay =[];
- var cardOne = cards[0];
- var cardTwo = cards[1];
- cardsInPlay.push(cardOne);
- cardsInPlay.push(cardTwo);
  
- console.log("User flipped "+ cardsInPlay[0]);
- console.log("User flipped "+ cardsInPlay[1]);
-
- if(cardsInPlay.length === 2)
+ var checkForMatch = function() 
  {
- 	  if (cardsInPlay[0] === cardsInPlay[1]) {
+    if (cardsInPlay[0] === cardsInPlay[1]) 
+    {
         alert('You found a match!');
     }
-    else {
+    else 
+    {
         alert('Sorry, try again.');
     }
+  }
+
+ var flipCard = function(cardId)
+ { 
+   
+   cardsInPlay.push(cards[cardId]);
+   console.log("User flipped " + cards[cardId]);
+   if(cardsInPlay.length === 2)
+   {
+     checkForMatch();
+   }  
  }
+flipCard(1);
+flipCard(3);
